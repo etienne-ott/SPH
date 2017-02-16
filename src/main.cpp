e1e7b5e576afa10cde252e1dfc39bca1151fab0d
Etienne Ott <eti.ott@gmail.com>
2017-02-16 12:54:39 +0100

    Add simple render function for the particle positions within a simple time loop.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <chrono>
 #include <cmath>
+#include "renderer.hpp"
 
 double rand(std::default_random_engine& engine) {
     return (double)engine() / engine.max();
@@ -44,10 +45,36 @@ void initFields(int N, double* position, double* velocity, double* density) {
     }
 }
 
+void renderPositions(double* pos, Renderer* r, int N, int R) {
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < R; j++) {
+            r->SetPixelRGB(i, j, 255, 255, 255);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        int iposx = (int)(R * pos[i * 3]),
+            iposy = (int)(R * pos[i * 3 + 1]),
+            zrange = (int)(10 * pos[i * 3 + 2]);
+        for (int k = iposx - zrange < 0 ? 0 : iposx - zrange; k < iposx + zrange && k < R; k++) {
+            for (int l = iposy - zrange < 0 ? 0 : iposy - zrange; l < iposy + zrange && l < R; l++) {
+                r->SetPixelRGB(k, l, 0, 0, 0);
+            }
+        }
+    }
+}
+
 int main() {
     int N = 20;
+    int R = 200;
     double h = 0.3;
+    double t = 0.0;
+    double tend = 50.0;
+    double dt = 0.1;
     double mass = 1.0 / N;
+
+    Renderer r = Renderer();
+    r.Init(R, R);
 
     double* position = new double[3 * N];
     double* velocity = new double[3 * N];
@@ -55,27 +82,51 @@ int main() {
 
     initFields(N, position, velocity, density); 
 
-    printField(velocity, N, 3);
-    printf("\n");
+    renderPositions(position, &r, N, R);
+    r.Render();
 
-    for (int i = 0; i < N; i++) {
-        double sum = 0.0;
-        double distance = 0.0;
-        for (int j = 0; j < N; j++) {
-            distance = pow(
-                (position[j * 3] - position[i * 3]) * (position[j * 3] - position[i * 3])
-                    + (position[j * 3 + 1] - position[i * 3 + 1]) * (position[j * 3 + 1] - position[i * 3 + 1])
-                    + (position[j * 3 + 2] - position[i * 3 + 2]) * (position[j * 3 + 2] - position[i * 3 + 2]),
-                0.5
-            );
-            sum += mass * kernel(distance, h, N);
+    while (t < tend) {
+        // Calculate density
+        for (int i = 0; i < N; i++) {
+            double sum = 0.0;
+            double distance = 0.0;
+            for (int j = 0; j < N; j++) {
+                distance = pow(
+                    (position[j * 3] - position[i * 3]) * (position[j * 3] - position[i * 3])
+                        + (position[j * 3 + 1] - position[i * 3 + 1]) * (position[j * 3 + 1] - position[i * 3 + 1])
+                        + (position[j * 3 + 2] - position[i * 3 + 2]) * (position[j * 3 + 2] - position[i * 3 + 2]),
+                    0.5
+                );
+                sum += mass * kernel(distance, h, N);
+            }
+            density[i] = sum;
         }
-        density[i] = sum;
+
+        // Do position integration (explicit euler)
+        for (int i = 0; i < N; i++) {
+            position[i * 3] += velocity[i * 3] * dt;
+            position[i * 3 + 1] += velocity[i * 3 + 1] * dt;
+            position[i * 3 + 2] += velocity[i * 3 + 2] * dt;
+        }
+
+        renderPositions(position, &r, N, R);
+        r.Render();
+        SDL_Delay(30);
+
+        t += dt;
     }
 
-    printField(density, N, 1);
+    bool running = true;
+    SDL_Event event;
 
-    printf("All finished\n");
+    while (running) {
+        SDL_Delay(30);
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
 
     delete[] position;
     delete[] velocity;

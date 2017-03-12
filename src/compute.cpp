8e4a9259f5cbb4f2829c889f81fbffa5d3901977
Etienne Ott <eti.ott@gmail.com>
2017-03-12 16:10:22 +0100

    Switch from explicit euler to leap from integration scheme.
    This has several advantages, even if it introduces additional variables in the form of velocity half steps.
    The velocity for the viscosity force needs to be calculated for timestep i, whereas the velocity and
    position integration now work with the velocities of timesteps i + 1/2. The advantages are a higher
    accuracy of order two and the use of a symplectic scheme, which avoids the energy loss (or build-up)
    associated with the explicit or implicit euler schemes.

--- src/compute.cpp
+++ src/compute.cpp
@@ -7,10 +7,13 @@ Compute::Compute(Parameter* param, Kernel* kernel) {
     _param = param;
     _kernel = kernel;
 
+    _isFirstStep = true;
+
     _vec1 = new double[3];
     _vec2 = new double[3];
     _matr1 = new double[9];
     _position = new double[3 * _param->N];
+    _velocity_halfs = new double[3 * _param->N];
     _velocity = new double[3 * _param->N];
     _force = new double[3 * _param->N];
     _density = new double[_param->N];
@@ -51,6 +54,7 @@ Compute::Compute(Parameter* param, Kernel* kernel) {
 Compute::~Compute() {
     delete[] _position;
     delete[] _velocity;
+    delete[] _velocity_halfs;
     delete[] _force;
     delete[] _density;
     delete[] _pressure;
@@ -84,17 +88,27 @@ double Compute::CalculateDensity() {
 }
 
 void Compute::Timestep() {
-    // h should be set to avg^(-1/d), where avg is the average density of the
-    // fluid and d is the dimension
+    // Calculate density and update smoothing length. h should be set to
+    // avg^(-1/d), where avg is the average density of the fluid and d is the
+    // number of dimensions.
     double h = pow(this->CalculateDensity(), -1.0 / 3.0);
     _kernel->SetH(h);
 
-    // Calculate pressure
+    this->CalculatePressure();
+    this->CalculateForces();
+    this->VelocityIntegration(_isFirstStep);
+    this->PositionIntegration();
+
+    _isFirstStep = false;
+}
+
+void Compute::CalculatePressure() {
     for (int i = 0; i < _param->N; i++) {
         _pressure[i] = _param->k * (pow(_density[i] / _param->rho0, 7.0) - 1);
     }
+}
 
-    // Calculate forces on particle i
+void Compute::CalculateForces() {
     double distance = 0.0;
     double tmp = 0.0;
 
@@ -103,17 +117,23 @@ void Compute::Timestep() {
         _force[i * 3 + 1] = 0.0;
         _force[i * 3 + 2] = 0.0;
 
+        // 1-body forces, currently only gravity
+        _force[i * 3 + 2] += _density[i] * _param->mass * _param->g * _param->FSGravity;
+
+        // 2-body forces
+        // @todo Use symmetry to reduce number of calculations by a factor of 2
         for (int j = 0; j < _param->N; j++) {
             if (j == i) {
                 continue;
             }
 
+            // Calculate distance vector
             _vec1[0] = _position[i * 3] - _position[j * 3];
             _vec1[1] = _position[i * 3 + 1] - _position[j * 3 + 1];
             _vec1[2] = _position[i * 3 + 2] - _position[j * 3 + 2];
             distance = pow(_vec1[0] * _vec1[0] + _vec1[1] * _vec1[1] + _vec1[2] * _vec1[2], 0.5);
 
-            // pressure
+            // Pressure force
             _kernel->FOD(_vec1[0], _vec1[1], _vec1[2], distance, _vec2);
             tmp = 0.5 * (_pressure[i] + _pressure[j]) * (_param->mass / _density[j]);
 
@@ -121,7 +141,7 @@ void Compute::Timestep() {
             _force[i * 3 + 1] -= _param->FSPressure * tmp * _vec2[1];
             _force[i * 3 + 2] -= _param->FSPressure * tmp * _vec2[2];
 
-            // viscosity
+            // Viscosity force
             _kernel->SOD(_vec1[0], _vec1[1], _vec1[2], distance, _matr1);
             tmp = _param->FSViscosity * _param->mu * _param->mass / _density[j];
             _force[i * 3] += tmp * (
@@ -142,21 +162,33 @@ void Compute::Timestep() {
                 + (_velocity[j * 3 + 2] - _velocity[i * 3 + 2]) * _matr1[8]
                 );
         }
+    }
+}
 
-        // gravity
-        _force[i * 3 + 2] += _density[i] * _param->mass * _param->g * _param->FSGravity;
+void Compute::VelocityIntegration(bool firstStep) {
+    if (firstStep) {
+        for (int i = 0; i < _param->N; i++) {
+            _velocity_halfs[i * 3] = _velocity[i * 3] +  0.5 + _param->dt * _force[i * 3] / _density[i];
+            _velocity_halfs[i * 3 + 1] = _velocity[i * 3 + 1] +  0.5 + _param->dt * _force[i * 3 + 1] / _density[i];
+            _velocity_halfs[i * 3 + 2] = _velocity[i * 3 + 2] +  0.5 + _param->dt * _force[i * 3 + 2] / _density[i];
+        }
+        return;
     }
 
-    // Do _velocity integration (explicit euler)
     for (int i = 0; i < _param->N; i++) {
-        _velocity[i * 3] += _param->dt * _force[i * 3] / _density[i];
-        _velocity[i * 3 + 1] += _param->dt * _force[i * 3 + 1] / _density[i];
-        _velocity[i * 3 + 2] += _param->dt * _force[i * 3 + 2] / _density[i];
+        _velocity_halfs[i * 3] += _param->dt * _force[i * 3] / _density[i];
+        _velocity_halfs[i * 3 + 1] += _param->dt * _force[i * 3 + 1] / _density[i];
+        _velocity_halfs[i * 3 + 2] += _param->dt * _force[i * 3 + 2] / _density[i];
+        _velocity[i * 3] = _velocity_halfs[i * 3] + 0.5 * _param->dt * _force[i * 3] / _density[i];
+        _velocity[i * 3 + 1] = _velocity_halfs[i * 3 + 1] + 0.5 * _param->dt * _force[i * 3 + 1] / _density[i];
+        _velocity[i * 3 + 2] = _velocity_halfs[i * 3 + 2] + 0.5 * _param->dt * _force[i * 3 + 2] / _density[i];
     }
+}
 
-    // Do _position integration (explicit euler) with collision detection
+void Compute::PositionIntegration() {
+    // Do _position integration (leap frog) with collision detection
     // against the standard rectangle spanned by (0,0,0)x(1,1,1), that is
-    // open on the upper side (z > 1).
+    // open on the upper side (z > 1) and extended infinitely.
     // On collision we reflect the respective component and rescale the
     // velocity so the new absolute velocity is the old one multiplied
     // with a dampening factor.
@@ -172,48 +204,48 @@ void Compute::Timestep() {
         bool damp = false;
 
         // Reflection of x component
-        newval = _position[i * 3] + _velocity[i * 3] * _param->dt;
+        newval = _position[i * 3] + _velocity_halfs[i * 3] * _param->dt;
         if (newval < 0.0) {
             damp = true;
             _position[i * 3] = -newval;
-            _velocity[i * 3] = -_velocity[i * 3];
+            _velocity_halfs[i * 3] = -_velocity_halfs[i * 3];
         } else if (newval > 1.0) {
             damp = true;
             _position[i * 3] = 2.0 - newval;
-            _velocity[i * 3] = -_velocity[i * 3];
+            _velocity_halfs[i * 3] = -_velocity_halfs[i * 3];
         } else {
             _position[i * 3] = newval;
         }
 
         // Reflection of y component
-        newval = _position[i * 3 + 1] + _velocity[i * 3 + 1] * _param->dt;
+        newval = _position[i * 3 + 1] + _velocity_halfs[i * 3 + 1] * _param->dt;
         if (newval < 0.0) {
             damp = true;
             _position[i * 3 + 1] = -newval;
-            _velocity[i * 3 + 1] = -_velocity[i * 3 + 1];
+            _velocity_halfs[i * 3 + 1] = -_velocity_halfs[i * 3 + 1];
         } else if (newval > 1.0) {
             damp = true;
             _position[i * 3 + 1] = 2.0 - newval;
-            _velocity[i * 3 + 1] = -_velocity[i * 3 + 1];
+            _velocity_halfs[i * 3 + 1] = -_velocity_halfs[i * 3 + 1];
         } else {
             _position[i * 3 + 1] = newval;
         }
 
         // Reflection of z component
-        newval = _position[i * 3 + 2] + _velocity[i * 3 + 2] * _param->dt;
+        newval = _position[i * 3 + 2] + _velocity_halfs[i * 3 + 2] * _param->dt;
         if (newval < 0.0) {
             damp = true;
             _position[i * 3 + 2] = -newval;
-            _velocity[i * 3 + 2] = -_velocity[i * 3 + 2];
+            _velocity_halfs[i * 3 + 2] = -_velocity_halfs[i * 3 + 2];
         } else {
             _position[i * 3 + 2] = newval;
         }
 
         // Dampening
         if (damp) {
-            _velocity[i * 3] *= _param->dampening;
-            _velocity[i * 3 + 1] *= _param->dampening;
-            _velocity[i * 3 + 2] *= _param->dampening;
+            _velocity_halfs[i * 3] *= _param->dampening;
+            _velocity_halfs[i * 3 + 1] *= _param->dampening;
+            _velocity_halfs[i * 3 + 2] *= _param->dampening;
         }
     }
 }
--- src/compute.hpp
+++ src/compute.hpp
@@ -23,6 +23,25 @@ public:
     /// @return double The average density of the fluid
     double CalculateDensity();
 
+    /// Calculates the pressure at the particle positions.
+    void CalculatePressure();
+
+    /// Calculates the forces that act on the particles. This consists of
+    /// multiple factors like gravity, pressure and viscosity.
+    void CalculateForces();
+
+    /// Does the velocity integration for the leap-frog integration scheme.
+    ///
+    /// @param firstStep bool Flag if we are doing the first time step, which
+    ///   requires some special handling
+    void VelocityIntegration(bool firstStep);
+
+    /// Does the position integration for the leap-frog integration scheme. If
+    /// a particle would leave the domain boundaries, it will be reflected
+    /// instead and a dampening force applied. In the default case the boundaries
+    /// are the unit box that is infinitely extended towards the positive z axis.
+    void PositionIntegration();
+
     /// Calculates one timestep of the fluid simulations, which includes
     /// calculating the forces on each particles, enforcing boundary
     /// conditions, then integrating the particle velocities and positions
@@ -48,6 +67,10 @@ private:
     /// @var _kernel Kernel* The kernel to use for calculations.
     Kernel* _kernel;
 
+    /// @var _isFirstStep bool Flag to indicate if we are doing the first time
+    ///     step, which requires special handling.
+    bool _isFirstStep;
+
     /// @var _vec1 double* A temporary 3D vector used in calculations.
     double* _vec1;
 
@@ -63,6 +86,9 @@ private:
 
     /// @var _velocity double* The particle velocities in x, y and z components.
     double* _velocity;
+
+    /// @var _velocity double* The particle velocities in x, y and z components.
+    double* _velocity_halfs;
 
     /// @var _force double* The sum of all forces acting on the particles in x,
     ///     y and z components.

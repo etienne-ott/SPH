5d7477efe3a16fbd35352fa3eacefd7aeceea7fc
Etienne Ott <eti.ott@gmail.com>
2017-03-31 12:45:09 +0200

    Add small disturbances to initial positions and velocities.
    This serves the purpose of breaking symmetry as early as the first time step. Some
    of the equations of the particle forces have unwanted behaviour if the difference
    in position or velocity between two particles is exactly zero. During the normal run
    this is not much of a problem since the difference is rarely zero. Hpwever during
    initialisation this occurs much more frequently.

--- src/compute.cpp
+++ src/compute.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include "compute.hpp"
 #include "parameter.hpp"
+#include "random_pool.hpp"
 
 Compute::Compute(Parameter* param, Kernel* kernel) {
     _param = param;
@@ -23,11 +24,12 @@ Compute::Compute(Parameter* param, Kernel* kernel) {
     double r = _param->h / _param->kappa, x = 0, y = 0, z = 0,
         offsetX = r, offsetY = r, offsetZ = r;
     bool oddRow = true, oddLayer = true;
+    RandomPool pool = RandomPool();
 
     for (int i = 0; i < _param->N; i++) {
-        _position[i * 3] = offsetX + x;
-        _position[i * 3 + 1] = offsetY + y;
-        _position[i * 3 + 2] = offsetZ + z;
+        _position[i * 3] = offsetX + x + pool.NextDouble(0.0, 0.00001);
+        _position[i * 3 + 1] = offsetY + y + pool.NextDouble(0.0, 0.00001);
+        _position[i * 3 + 2] = offsetZ + z + pool.NextDouble(0.0, 0.00001);
 
         x += 2 * r;
         if (offsetX + x > 1.0 - r) {
@@ -47,9 +49,9 @@ Compute::Compute(Parameter* param, Kernel* kernel) {
             z += sqrt(3) * r;
         }
 
-        _velocity[i * 3] = 0.0;
-        _velocity[i * 3 + 1] = 0.0;
-        _velocity[i * 3 + 2] = 0.0;
+        _velocity[i * 3] = pool.NextDouble(0.0, 0.001);
+        _velocity[i * 3 + 1] = pool.NextDouble(0.0, 0.001);
+        _velocity[i * 3 + 2] = pool.NextDouble(0.0, 0.001);
 
         _force[i * 3] = 0.0;
         _force[i * 3 + 1] = 0.0;

790e0517d1cdc6276c208b0fe1f7055000e5f112
Etienne Ott <eti.ott@gmail.com>
2018-06-29 14:13:08 +0200

    Switch to simple random starting positions for testing purposes.

--- src/initialization.cpp
+++ src/initialization.cpp
@@ -6,33 +6,12 @@ Initialization::Initialization(YAML::Node& param) {
 }
 
 void Initialization::InitPosition(float* position) {
-    float r = _param["h"].as<float>() / _param["kappa"].as<float>(), x = 0, y = 0, z = 0,
-        offsetX = r - 0.5 * r, offsetY = r - 0.5 * r, offsetZ = r - 0.5 * r;
-    bool oddRow = true, oddLayer = true;
     RandomPool pool = RandomPool();
 
     for (int i = 0; i < _param["N"].as<int>(); i++) {
-        position[i * 3] = offsetX + x + pool.NextFloat(0.0, 0.00001);
-        position[i * 3 + 1] = offsetY + y + pool.NextFloat(0.0, 0.00001);
-        position[i * 3 + 2] = offsetZ + z + pool.NextFloat(0.0, 0.00001);
-
-        x += r;
-        if (offsetX + x > 1.0 - r * 0.25) {
-            oddRow = !oddRow;
-            x = 0;
-            offsetX = oddLayer ? (oddRow ? 0.5 * r : r) : (oddRow ? r : 0.5 * r);
-            y += sqrt(3) * r * 0.5;
-        }
-
-        if (offsetY + y > 1.0 - r * 0.25) {
-            oddLayer = !oddLayer;
-            oddRow = true;
-            x = 0;
-            y = 0;
-            offsetX = oddLayer ? 0.5 * r : r;
-            offsetY = oddLayer ? 0.5 * r: (sqrt(3) - 1) * r * 0.5;
-            z += sqrt(3) * r * 0.5;
-        }
+        position[i * 3] = pool.NextFloat(0.5f, 1.f);
+        position[i * 3 + 1] = pool.NextFloat(0.5f, 1.f);
+        position[i * 3 + 2] = pool.NextFloat(0.5f, 1.f);
     }
 }
 
